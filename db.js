b94c87bf092049a0902f82e6476d3df6f0a5264a
Christopher Milan <chris.m.milan@gmail.com>
2023-01-12 11:51:20 -0800

    add tests for iam

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const connectionString = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.7tfqkhl.mongodb.net/?retryWrites=true&w=majority`
+const connectionString = process.env.MONGO_URI;
 const client = new MongoClient(connectionString);
 
 const db = client.db("datares-discord-bot");
--- test/iam.test.js
+++ test/iam.test.js
@@ -1,5 +1,47 @@
+const { MongoClient } = require('mongodb');
+const mockSES = require("aws-sdk/clients/ses");
 const iam = require("../commands/iam");
 
-test("example", () => {
-    expect(true).toBe(true);
+jest.mock("aws-sdk/clients/ses", () => {
+    const mSES = {
+        sendEmail: jest.fn().mockReturnThis().mockName("sendEmail"),
+        promise: jest.fn().mockName("promise"),
+    };
+    return jest.fn(() => mSES);
 });
+
+describe("iam", () => {
+    describe("given invalid email", () => {
+        test("returns 'invalid email'", async () => {
+            expect(await iam("not an email", 0)).toBe("invalid email");
+            expect(await iam("jbruin", 0)).toBe("invalid email");
+        })
+    });
+    describe("with valid email", () => {
+        let client;
+        let db;
+        beforeAll(() => {
+            client = new MongoClient(global.__MONGO_URI__);
+            db = client.db("datares-discord-bot");
+        });
+        afterAll(async () => {
+            await client.close();
+        });
+        test("sends email", async () => {
+            const mSES = new mockSES();
+            expect(await iam("chris.m.milan@gmail.com", 0)).toBe("success");
+            expect(mSES.sendEmail).toHaveBeenCalled();
+        });
+        test("stores verification code", async () => {
+            let email = "chris.m.milan@gmail.com";
+            let user_id = 0;
+
+            expect(await iam(email, user_id)).toBe("success");
+            
+            expect(await db.collection("verification").findOne({user_id})).toEqual(expect.objectContaining({
+                email,
+                user_id
+            }));
+        });
+    })
+})
