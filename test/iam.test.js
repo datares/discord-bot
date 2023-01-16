5b489a13eb7feac8130b32823a1355e65cb53c45
Christopher Milan <chris.m.milan@gmail.com>
2023-01-16 15:58:03 -0800

    update tests for refactor

--- test/iam.test.js
+++ test/iam.test.js
@@ -1,47 +1,71 @@
 const { MongoClient } = require('mongodb');
-const mockSES = require("aws-sdk/clients/ses");
-const iam = require("../commands/iam");
+const mockSES = require('aws-sdk/clients/ses');
+const { execute } = require('../commands/iam');
 
-jest.mock("aws-sdk/clients/ses", () => {
+jest.mock('aws-sdk/clients/ses', () => {
     const mSES = {
-        sendEmail: jest.fn().mockReturnThis().mockName("sendEmail"),
-        promise: jest.fn().mockName("promise"),
+        sendEmail: jest.fn().mockReturnThis().mockName('sendEmail'),
+        promise: jest.fn().mockName('promise'),
     };
     return jest.fn(() => mSES);
 });
 
-describe("iam", () => {
-    describe("given invalid email", () => {
-        test("returns 'invalid email'", async () => {
-            expect(await iam("not an email", 0)).toBe("invalid email");
-            expect(await iam("jbruin", 0)).toBe("invalid email");
+describe('iam', () => {
+    let interaction;
+    beforeAll(() => {
+        interaction = {
+            options: {
+                get: jest.fn().mockImplementation((name) => {
+                    return {
+                        value: interaction.options[name]
+                    }
+                }),
+            },
+            member: {
+                user: {
+                    id: 0,
+                }
+            },
+            reply: jest.fn()
+        }
+    })
+    describe('given invalid email', () => {
+        test('sends error message', async () => {
+            interaction.options.email = 'not an email';
+            await execute(interaction);
+            expect(interaction.reply).toHaveBeenCalledWith({
+                content: 'Your email doesn\'t appear to be a valid email. Please try again',
+                ephemeral: true,
+            });
         })
     });
-    describe("with valid email", () => {
-        let client;
-        let db;
-        beforeAll(() => {
-            client = new MongoClient(global.__MONGO_URI__);
-            db = client.db("datares-discord-bot");
-        });
-        afterAll(async () => {
-            await client.close();
-        });
-        test("sends email", async () => {
+    describe('with valid email', () => {
+        test('sends email', async () => {
             const mSES = new mockSES();
-            expect(await iam("chris.m.milan@gmail.com", 0)).toBe("success");
+            interaction.options.email = 'chris.m.milan@gmail.com';
+            await execute(interaction);
+            expect(interaction.reply).toHaveBeenCalledWith({
+                content: 'Please check your email address for an authorization code',
+                ephemeral: true
+            });
             expect(mSES.sendEmail).toHaveBeenCalled();
         });
-        test("stores verification code", async () => {
-            let email = "chris.m.milan@gmail.com";
-            let user_id = 0;
+        test('stores verification code', async () => {
+            interaction.options.email = 'chris.m.milan@gmail.com';
+            let client = new MongoClient(global.__MONGO_URI__);
+            let db = client.db('datares-discord-bot');
 
-            expect(await iam(email, user_id)).toBe("success");
-            
-            expect(await db.collection("verification").findOne({user_id})).toEqual(expect.objectContaining({
-                email,
-                user_id
+            await execute(interaction);
+
+            expect(interaction.reply).toHaveBeenCalledWith({
+                content: 'Please check your email address for an authorization code',
+                ephemeral: true
+            });
+            expect(await db.collection('verification').findOne({user_id: 0})).toEqual(expect.objectContaining({
+                email: 'chris.m.milan@gmail.com',
+                user_id: 0
             }));
+            await client.close();
         });
     })
 })
