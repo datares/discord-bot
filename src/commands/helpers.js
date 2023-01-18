6ee3b37b2fa80f6f9b7a6feb6cafa5221d1e34b7
Christopher Milan <chris.m.milan@gmail.com>
2023-01-17 21:39:58 -0800

    set user nicknames in update-roles and verify

--- src/commands/helpers.js
+++ src/commands/helpers.js
@@ -1,16 +1,5 @@
 const readSheet = require("./read_sheet");
 
-const getRoles = async (email) => {
-  const members = await readSheet();
-  const user = members.find((value) => {
-    return value.email == email;
-  });
-  if (!user) {
-    return null;
-  }
-  return user.status;
-};
-
 const getMember = (client, userId) => {
   const guild = client.guilds.cache.get(process.env.GUILD_ID);
   const member = guild.members.cache.get(userId);
@@ -27,9 +16,28 @@ const addRoleToUser = async (client, user_id, role_to_assign) => {
   return role_to_assign;
 };
 
-const updateUserRoles = async (email, user_id, client) => {
-  const role_to_assign = await getRoles(email);
-  return addRoleToUser(client, user_id, role_to_assign);
-};
+async function updateUserFromSheet(email, user_id, client) {
+  const members = await readSheet();
+  const user = members.find((value) => value.email === email);
 
-module.exports = { updateUserRoles, addRoleToUser };
+  // If we could not find user
+  if (!user) {
+    return null;
+  }
+
+  // Give user respective team role
+  const member = getMember(client, user_id);
+  const role = member.guild.roles.cache.find((r) => r.name === user.status);
+  member.roles.add(role);
+
+  // Set user's name
+  try {
+    member.setNickname(user.name);
+  } catch (err) {
+    console.error(`Failed to set ${user.name}'s nickname: `, err);
+  }
+
+  return user.status;
+}
+
+module.exports = { updateUserFromSheet, addRoleToUser };
